1e02f2a5922a5a4cfe47b1585dcbc69fd2cc471b
Kokogawa Yuri <peaceproject19951226@gmail.com>
2020-11-18 11:05:07 +0000

    mynews

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// 10 ControllerとViewが連携できるようにしようのカリキュラム
+Route::get('admin/news/create','Admin\NewsController@add');
+// 10 ControllerとViewが連携できるようにしようの課題
+Route::get('admin/profile/create','Admin\ProfileController@add');
+Route::get('admin/profile/edit','Admin\ProfileController@edit');
+
+
+// 課題４
+Route::get('XXX','Admin/AAAcontroller@bbb');
+
+// 課題５
+Route::get('admin/profile/create','Admin\ProfileController@add');
+Route::get('admin/profile/edit','Admin\ProfileController@edit');
+
