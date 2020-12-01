5693b2cd3920fa16331bbd1dafe4aa82fe9f7bb9
Kokogawa Yuri <peaceproject19951226@gmail.com>
2020-12-01 05:17:43 +0000

    メッセージ

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\Controller;
 
 // 以下を追記することでNews Modelが扱えるようになる
 use App\News;
-
+use App\History;
+use Carbon\Carbon;
 class NewsController extends Controller
 {
   public function add()
@@ -84,6 +85,12 @@ class NewsController extends Controller
 
       // 該当するデータを上書きして保存する
       $news->fill($news_form)->save();
+      
+       // 以下を追記
+        $history = new History;
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
 
       return redirect('admin/news');
   }
